37b3e287d6e5c245ed83ffb11165015939e24a55
saikiranyad <saisaikiran7890@gmail.com>
2024-10-06 16:29:31 +0530

    Update connecttodb.js

--- Backend/db/connecttodb.js
+++ Backend/db/connecttodb.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const mongoUri = async()=>{
     try{
-await mongoose.connect(process.env.MONGO_URI);
+await mongoose.connect("mongodb+srv://Sai7890:Sai7890@cluster0.zlhgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 console.log("connected to db")
     }catch(err){
         console.log("not connected to db due to ",err)
